515d75e3952d732dcc6083a2a2e6f1ab63dc9cc6
James Knight <knightjdr@gmail.com>
2021-02-05 15:33:17 -0500

    Add gorouting for scatter generation

--- pkg/tools/analyze/specificity/writeimages.go
+++ pkg/tools/analyze/specificity/writeimages.go
@@ -2,20 +2,29 @@ package specificity
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/knightjdr/prohits-viz-analysis/pkg/svg"
 	"github.com/knightjdr/prohits-viz-analysis/pkg/types"
 )
 
 func writeImages(data map[string][]types.ScatterPoint, settings types.Settings) {
-	for condition, conditionData := range data {
-		filehandle := fmt.Sprintf("specificity-%s", condition)
-		createSVG(conditionData, settings, filehandle)
+	var wg sync.WaitGroup
+	wg.Add(len(data))
 
-		if settings.Png {
-			svg.ConvertToPNG(fmt.Sprintf("svg/%s.svg", filehandle), fmt.Sprintf("png/%s.png", filehandle), "white")
-		}
+	for condition, conditionData := range data {
+		go func(condition string, conditionData []types.ScatterPoint) {
+			defer wg.Done()
+			filehandle := fmt.Sprintf("specificity-%s", condition)
+			createSVG(conditionData, settings, filehandle)
+
+			if settings.Png {
+				svg.ConvertToPNG(fmt.Sprintf("svg/%s.svg", filehandle), fmt.Sprintf("png/%s.png", filehandle), "white")
+			}
+		}(condition, conditionData)
 	}
+
+	wg.Wait()
 }
 
 func createSVG(data []types.ScatterPoint, settings types.Settings, filehandle string) {
